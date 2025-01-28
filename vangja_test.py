5fb4cd1a70d605ace4c43ec42c57fbf78fb5d3fb
Jovan Krajevski <jovan.krajevski@gmail.com>
2025-01-28 23:39:49 +0100

    +

--- vangja_test.py
+++ vangja_test.py
@@ -3,13 +3,12 @@ from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
 
-from vangja.components import FourierSeasonality, LinearTrend
-from vangja.components.beta_constant import BetaConstant
 from vangja.data_utils import (
     download_data,
     generate_train_test_df_around_point,
     process_data,
 )
+from vangja_simple.components import BetaConstant, FourierSeasonality, LinearTrend
 
 print("START")
 
@@ -26,7 +25,12 @@ model = LinearTrend() * (
     * FourierSeasonality(7, 3, allow_tune=True, tune_method="simple")
 )
 model.load_trace(Path("./") / "models" / "trace_40_y_bc_w.nc")
-model.fit_params["map_approx"] = None
+for key in model.fit_params["trace"]["posterior"]:
+    if key.startswith("fs_"):
+        model.fit_params["trace"]["posterior"][key] = model.fit_params["trace"][
+            "posterior"
+        ][key][:, :, 0, :]
+# model.fit_params["map_approx"] = None
 
 # model.left = LinearTrend(pool_cols="series", pool_type="individual")
 # model.right.left = FourierSeasonality(
@@ -40,7 +44,7 @@ model.fit_params["map_approx"] = None
 #     pool_cols="series",
 #     pool_type="individual",
 # )
-for point in pd.date_range("2015-01-01", "2019-01-01"):
+for point in pd.date_range("2015-01-02", "2019-01-01"):
     points = f"{point.year}-{'' if point.month > 9 else '0'}{point.month}-{'' if point.day > 9 else '0'}{point.day}"
     model_metrics = []
     for gspc_ticker in tqdm(gspc_tickers):
@@ -57,8 +61,8 @@ for point in pd.date_range("2015-01-01", "2019-01-01"):
         train_df_tickers, test_df_tickers, scales_tickers = check
         model.tune(train_df_tickers, progressbar=False)
         yhat = model.predict(365)
-        model_metrics.append(model.metrics(test_df_tickers, yhat, pool_cols="series"))
+        model_metrics.append(model.metrics(test_df_tickers, yhat))
 
     final_metrics = pd.concat(model_metrics)
     final_metrics.to_csv(Path("./") / "out" / "vangja" / "test" / "{points}.csv")
-    print(f"{model}: {final_metrics['mape'].mean()}")
+    print(f"{final_metrics['mape'].mean()}")
