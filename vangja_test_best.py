e3e3b4e3e453884c649de2d8ab408211d3fb3d2a
Jovan Krajevski <jovan.krajevski@gmail.com>
2025-02-23 15:03:55 -0500

    +

--- vangja_test_best.py
+++ vangja_test_best.py
@@ -136,5 +136,5 @@ for point in pd.date_range(f"{year_start}-01-01", f"{year_end}-01-01"):
 
     del model
     gc.collect()
-    jax.clear_backends()
+    # jax.clear_backends()
     jax.clear_caches()
