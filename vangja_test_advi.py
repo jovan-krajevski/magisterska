cee3afd41ad38efca103e707009fe1477fd6bf88
Jovan Krajevski <jovan.krajevski@gmail.com>
2025-03-14 03:02:07 +0100

    +

--- vangja_test_advi.py
+++ vangja_test_advi.py
@@ -16,12 +16,9 @@ from vangja.data_utils import (
     process_data,
 )
 from vangja_simple.components import (
-    BetaConstant,
-    Constant,
     FourierSeasonality,
     LinearTrend,
 )
-from vangja_simple.components.normal_constant import NormalConstant
 
 parser = argparse.ArgumentParser(
     prog="Vangja Test", description="Run Vangja on test set", epilog="---"
@@ -43,23 +40,11 @@ gspc_tickers = process_data(dfs[1])
 
 print("DATA READY")
 
-prophet_trend = LinearTrend(n_changepoints=0)
-prophet_weekly = FourierSeasonality(7, 3)
-prophet_model = prophet_trend**prophet_weekly
-
-
-# for key in model.fit_params["trace"]["posterior"]:
-#     if key.startswith("fs_"):
-#         model.fit_params["trace"]["posterior"][key] = model.fit_params["trace"][
-#             "posterior"
-#         ][key][:, :, 0, :]
-
 scores = []
-
 
 for point in pd.date_range(f"{year_start}-01-01", f"{year_end}-01-01"):
     points = f"{point.year}-{'' if point.month > 9 else '0'}{point.month}-{'' if point.day > 9 else '0'}{point.day}"
-    parent_path = Path("./") / "out" / "vangja" / "test202"
+    parent_path = Path("./") / "out" / "vangja" / "test204"
     csv_path = parent_path / f"{points}.csv"
     maps_path = parent_path / f"{points}_maps.csv"
     csv_path.parent.mkdir(parents=True, exist_ok=True)
@@ -69,14 +54,10 @@ for point in pd.date_range(f"{year_start}-01-01", f"{year_end}-01-01"):
         continue
 
     train_df_smp, test_df_smp, scales_smp = generate_train_test_df_around_point(
-        window=365 * 40, horizon=365, dfs=smp, for_prophet=False, point=point
+        window=365 * 40, horizon=365, dfs=smp, for_prophet=False, point=points
     )
     trend = LinearTrend(changepoint_range=1)
-    # presidential = FourierSeasonality(
-    #     365.25 * 4, 9, allow_tune=True, tune_method="simple"
-    # )
     yearly = FourierSeasonality(365.25, 10, allow_tune=True, tune_method="simple")
-    # quarterly = FourierSeasonality(365.25 / 4, 5, allow_tune=True, tune_method="simple")
     weekly = FourierSeasonality(7, 3, allow_tune=True, tune_method="simple")
     model = trend ** (weekly + yearly)
     model.fit(train_df_smp)
@@ -85,65 +66,35 @@ for point in pd.date_range(f"{year_start}-01-01", f"{year_end}-01-01"):
     weekly_mean = model.map_approx[
         f"fs_{weekly.model_idx} - beta(p={weekly.period},n={weekly.series_order})"
     ]
-    # quarterly_mean = model.map_approx[
-    #     f"fs_{quarterly.model_idx} - beta(p={quarterly.period},n={quarterly.series_order})"
-    # ]
     yearly_mean = model.map_approx[
         f"fs_{yearly.model_idx} - beta(p={yearly.period},n={yearly.series_order})"
     ]
-    # presidential_mean = model.map_approx[
-    #     f"fs_{presidential.model_idx} - beta(p={presidential.period},n={presidential.series_order})"
-    # ]
 
     model_metrics = []
     model_maps = []
     trend = LinearTrend(
         n_changepoints=0, allow_tune=True, override_slope_mean_for_tune=slope_mean
     )
-    # presidential = FourierSeasonality(
-    #     365.25 * 4,
-    #     9,
-    #     allow_tune=True,
-    #     tune_method="simple",
-    #     override_beta_mean_for_tune=presidential_mean,
-    #     shift_for_tune=False,
-    #     shrinkage_strength=1,
-    # )
     yearly = FourierSeasonality(
         365.25,
         10,
         allow_tune=True,
-        tune_method="prior_from_idata",
+        tune_method="simple",
         override_beta_mean_for_tune=yearly_mean,
         shift_for_tune=False,
-        shrinkage_strength=1,
+        shrinkage_strength=10,
     )
-    # quarterly = FourierSeasonality(
-    #     365.25 / 4,
-    #     5,
-    #     allow_tune=True,
-    #     tune_method="simple",
-    #     override_beta_mean_for_tune=quarterly_mean,
-    #     shift_for_tune=False,
-    #     shrinkage_strength=100,
-    # )
     weekly = FourierSeasonality(
         7,
         3,
-        allow_tune=True,
-        tune_method="prior_from_idata",
+        allow_tune=False,
+        tune_method="simple",
         override_beta_mean_for_tune=weekly_mean,
         shift_for_tune=False,
         shrinkage_strength=1,
     )
-    constant = NormalConstant(1, 0.1)
     model = trend ** (weekly + yearly)
     model.load_model(Path("./") / "models" / "simple_advi" / f"{points}")
-    # model.scale_params = {
-    #     **model.scale_params,
-    #     "ds_min": scale_params["ds_min"],
-    #     "ds_max": scale_params["ds_max"],
-    # }
 
     min_smp_y = train_df_smp["y"].iloc[-91:].min()
     max_smp_y = train_df_smp["y"].iloc[-91:].max()
@@ -160,15 +111,13 @@ for point in pd.date_range(f"{year_start}-01-01", f"{year_end}-01-01"):
             continue
 
         train_df_tickers, test_df_tickers, scales_tickers = check
+
         min_y = train_df_tickers["y"].min()
         max_y = train_df_tickers["y"].max()
         if max_y != min_y:
             train_df_tickers["y"] = (train_df_tickers["y"] - min_y) / (
                 max_y - min_y
             ) * (max_smp_y - min_smp_y) + min_smp_y
-
-        # prophet_model.fit(train_df_tickers, progressbar=False)
-        # prophet_yhat = prophet_model.predict(365)
 
         model.tune(train_df_tickers, progressbar=False)
         yhat = model.predict(365)
@@ -177,37 +126,12 @@ for point in pd.date_range(f"{year_start}-01-01", f"{year_end}-01-01"):
             yhat["yhat"] = (yhat["yhat"] - min_smp_y) / (max_smp_y - min_smp_y) * (
                 max_y - min_y
             ) + min_y
-            # prophet_yhat["yhat"] = (prophet_yhat["yhat"] - min_smp_y) / (
-            #     max_smp_y - min_smp_y
-            # ) * (max_y - min_y) + min_y
 
-        # model_train_metrics = model.metrics(
-        #     train_df_tickers,
-        #     yhat[: -len(test_df_tickers)],
-        #     label=train_df_tickers["series"].iloc[0],
-        # )
-        # prophet_train_metrics = prophet_model.metrics(
-        #     train_df_tickers,
-        #     prophet_yhat[: -len(test_df_tickers)],
-        #     label=train_df_tickers["series"].iloc[0],
-        # )
         model_test_metrics = model.metrics(
             test_df_tickers, yhat, label=train_df_tickers["series"].iloc[0]
         )
-        # prophet_test_metrics = prophet_model.metrics(
-        #     test_df_tickers, prophet_yhat, label=train_df_tickers["series"].iloc[0]
-        # )
-
-        # if (
-        #     model_train_metrics["mape"].iloc[0]
-        #     < 2 * prophet_train_metrics["mape"].iloc[0]
-        # ):
         model_metrics.append(model_test_metrics)
-        # else:
-        #     model_metrics.append(prophet_test_metrics)
-
         model_maps.append(model.map_approx)
-        # print(model_metrics[-1]["mape"].iloc[0])
 
     final_metrics = pd.concat(model_metrics)
     final_maps = pd.DataFrame.from_records(model_maps, index=final_metrics.index)
