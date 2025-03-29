00d26b6b6d904808c67aaa7ddfcbce6676a1e024
Jovan Krajevski <jovan.krajevski@gmail.com>
2025-03-29 01:50:51 +0100

    +

--- run_hierarchical.py
+++ run_hierarchical.py
@@ -1,3 +1,5 @@
+import jax
+import gc
 from pathlib import Path
 from vangja.data_utils import (
     download_data,
@@ -16,15 +18,9 @@ indexes = process_data(dfs[0])
 smp = [index for index in indexes if index["series"].iloc[0] == "^GSPC"]
 gspc_tickers = process_data(dfs[1])
 
-trend = LinearTrend()
-yearly = FourierSeasonality(365.25, 10)
-weekly = FourierSeasonality(7, 3)
-model = trend ** (weekly + yearly)
 
 for shrinkage_strength in [100, 10, 50, 500, 1000]:
     scores = []
-    yearly.shrinkage_strength = shrinkage_strength
-    weekly.shrinkage_strength = shrinkage_strength
 
     for point in pd.date_range("2015-01-01", "2017-01-01"):
         points = f"{point.year}-{'' if point.month > 9 else '0'}{point.month}-{'' if point.day > 9 else '0'}{point.day}"
@@ -32,6 +28,7 @@ for shrinkage_strength in [100, 10, 50, 500, 1000]:
             Path("./") / "out" / "timeseers" / f"shrinkage_{shrinkage_strength}"
         )
         csv_path = parent_path / f"{points}.csv"
+        maps_path = parent_path / f"{points}_maps.csv"
         csv_path.parent.mkdir(parents=True, exist_ok=True)
         if csv_path.is_file():
             scores.append(pd.read_csv(csv_path, index_col=0).iloc[:-1]["mape"].mean())
@@ -47,6 +44,10 @@ for shrinkage_strength in [100, 10, 50, 500, 1000]:
         train_data = pd.concat([train_smp, train_df])
         test_data = pd.concat([test_smp, test_df])
 
+        trend = LinearTrend()
+        yearly = FourierSeasonality(365.25, 10, shrinkage_strength=shrinkage_strength)
+        weekly = FourierSeasonality(7, 3, shrinkage_strength=shrinkage_strength)
+        model = trend ** (weekly + yearly)
         model.fit(train_data, hierarchical_model=True)
         yhat = model.predict(365, True)
         metrics = model.hierarchical_metrics(test_data, yhat)
@@ -55,3 +56,10 @@ for shrinkage_strength in [100, 10, 50, 500, 1000]:
         scores.append(metrics.iloc[:-1]["mape"].mean())
         print(f"{metrics['mape'].mean()}")
         print(f"so far: {sum(scores) / len(scores)}")
+
+        maps = pd.DataFrame.from_records([model.map_approx], index=[0])
+        maps.to_csv(maps_path)
+
+        del model
+        gc.collect()
+        jax.clear_caches()
